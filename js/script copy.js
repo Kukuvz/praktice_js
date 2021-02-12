62cebcd14f189ba5a0440fbfb09e9a832b397bb4
Anton <antonslayer@gmail.com>
2021-02-12 13:22:38 +0300

    third praktice

--- js/script copy.js
+++ js/script copy.js
@@ -99,3 +99,66 @@ for (let i = 1; i < 10; i++) {
     console.log(i);
     num++;
 }
+
+let num5 = 20;
+
+function showFirstMessage(text) {
+    console.log(text);
+    console.log(num5);
+}
+
+showFirstMessage("Hello World!");
+console.log(num5);
+
+function calc(a, b) {
+    return (a + b);
+}
+
+console.log(calc(4, 3));
+console.log(calc(5, 6));
+console.log(calc(10, 6));
+
+function ret() {
+    let num = 50;
+    return num;
+}
+
+const anothernum = ret();
+console.log(anothernum);
+
+const logger = function () {
+    console.log("Hello");
+};
+
+logger();
+
+const calc1 = (a, b) => {
+    console.log();
+    return a + b;
+};
+
+const str = "teSt";
+const arr = [1, 2, 3];
+
+// console.log(str[2]);
+// console.log(str.toUpperCase());
+console.log(str.toLowerCase());
+console.log(str);
+
+const fruit = "Some fruit";
+console.log(fruit.indexOf("q"));
+
+const logg = "Hello world";
+// console.log(logg.slice(6, 11));
+
+// console.log(logg.substring(6, 11));
+
+console.log(logg.substr(6, 5));
+
+
+const num2 = 12.2;
+console.log(Math.round(num2));
+
+const test = "12.2px";
+console.log(parseInt(test));
+console.log(parseFloat(test));
--- js/script.js
+++ js/script.js
@@ -1,6 +1,15 @@
-"use strict";
+'use strict';
 
-const numberOfFilms = +prompt('Сколько фильмоы вы уже посмотрели?', '');
+let numberOfFilms;
+
+function start() {
+    numberOfFilms = +prompt('Сколько фильмоы вы уже посмотрели?', '');
+    while (numberOfFilms == '' || numberOfFilms == null || isNaN(numberOfFilms)) {
+        numberOfFilms = +prompt('Сколько фильмоы вы уже посмотрели?', '');
+    }
+}
+
+start();
 
 const personalMovieDB = {
     count: numberOfFilms,
@@ -10,31 +19,49 @@ const personalMovieDB = {
     privat: false
 };
 
-let i = 0;
-while (i < 2) {
-    const a = prompt('Один из последних просмотренных фильмов?', ''),
-        b = prompt('На сколько вы оцените его?', '');
+function rememberMyFilms() {
+    for (let i = 0; i < 2; i++) {
+        const a = prompt('Один из последних просмотренных фильмов?', ''),
+            b = prompt('На сколько вы оцените его?', '');
 
-    if (a != null && b != null && a != '' && b != '' && a.length < 50) {
-        personalMovieDB.movies[a] = b;
-        console.log("Done!");
-        i++;
-    } else {
-        console.log('Error');
-        if (i > 0) {
+        if (a != null && b != null && a != '' && b != '' && a.length < 50) {
+            personalMovieDB.movies[a] = b;
+            console.log("Done!");
+        } else {
+            console.log('Error');
             i--;
         }
     }
 }
 
-if (personalMovieDB.count < 10) {
-    console.log("Просмотрено довольно мало фильмов");
-} else if (personalMovieDB.count >= 10 && personalMovieDB.count < 30) {
-    console.log("Вы классический зритель");
-} else if (personalMovieDB.count >= 30) {
-    console.log("Вы киноман");
-} else {
-    console.log("Произошла ошибка");
+rememberMyFilms();
+
+function detectPersonalLevel() {
+    if (personalMovieDB.count < 10) {
+        console.log("Просмотрено довольно мало фильмов");
+    } else if (personalMovieDB.count >= 10 && personalMovieDB.count < 30) {
+        console.log("Вы классический зритель");
+    } else if (personalMovieDB.count >= 30) {
+        console.log("Вы киноман");
+    } else {
+        console.log("Произошла ошибка");
+    }
 }
 
-console.log(personalMovieDB);
+detectPersonalLevel();
+
+function showMyDB(hidden) {
+    if (!hidden) {
+        console.log(personalMovieDB);
+    }
+}
+
+showMyDB(personalMovieDB.privat);
+
+function writeYourGenres() {
+    for (let i = 1; i < 4; i++) {
+        personalMovieDB.genres[i - 1] = prompt(`Ваш любимый жанр под номером ${i}`, '');
+    }
+}
+
+writeYourGenres();
