5cdb76c173096ba14f1395ab1f80608473dac6e0
Anton Gaivoronskiy <gaivoronskiy.anton.i@gmail.com>
2021-03-03 19:10:08 +0300

    new changes

--- js/prakt.js
+++ js/prakt.js
@@ -187,3 +187,266 @@ console.log(Math.round(num55));
 const test = "12.2px";
 console.log(parseInt(test));
 console.log(parseFloat(test));
+
+
+function first() {
+	//Do something
+	setTimeout(function () {
+		console.log(1);
+	}, 500);
+}
+
+function second() {
+	console.log(2);
+}
+
+first();
+second();
+
+function learnJS(lang, callback) {
+	console.log(`Я учу: ${lang}`);
+	callback();
+}
+
+function done() {
+	console.log('Я прошел этот урок!');
+}
+
+learnJS('Javascript', done);
+
+
+const opt = {
+	name: 'test',
+	width: 1024,
+	height: 1024,
+	colors: {
+		border: 'black',
+		bg: 'red'
+	},
+	makeTest: function () {
+		console.log("Testing");
+	}
+};
+
+opt.makeTest();
+
+const {
+	border,
+	bg
+} = opt.colors;
+console.log(border);
+
+console.log(Object.keys(opt).length);
+
+// console.log(opt["colors"]["border"]);
+
+// delete opt.name;
+// console.log(opt);
+
+// let counter = 0;
+// for (let key in opt) {
+// 	if (typeof (opt[key]) === 'object') {
+// 		for (let i in opt[key]) {
+// 			console.log(`Свойство ${i} имеет значение ${opt[key][i]}`);
+// 		}
+// 	} else {
+// 		console.log(`Свойство ${key} имеет значение ${opt[key]}`);
+// 		counter++;
+// 	}
+// }
+// console.log(counter);
+
+const arra = [2, 13, 26, 8, 10];
+// arra.sort(compareNum);
+// console.log(arra);
+
+function compareNum(a, b) {
+	return a - b;
+}
+
+
+// arra[99] = 0;
+// console.log(arra.length);
+
+// console.log(arra);
+
+arra.forEach(function (item, i, arra) {
+	console.log(`${i}: ${item} внутри массива ${arra}`);
+});
+
+// arra.pop();
+// arra.push(10);
+
+// console.log(arra);
+
+// for (let i = 0; i < arra.length; i++) {
+// 	console.log(arra[i]);
+// }
+
+for (let value of arra) {
+	console.log(value);
+}
+
+const stt = prompt("", "");
+const prod = stt.split(", ");
+console.log(prod);
+
+
+let a = 5,
+	b = a;
+
+b = b + 5;
+
+console.log(b);
+console.log(a);
+
+// const od = {
+// 	a: 5,
+// 	b: 1
+// };
+
+// const copp = od;
+
+// copp.a = 10;
+
+// console.log(copp);
+// console.log(od);
+
+function copy(mainObj) {
+	let objCopy = {};
+
+	let key;
+	for (key in mainObj) {
+		objCopy[key] = mainObj[key];
+	}
+
+	return objCopy;
+}
+
+const numbers = {
+	a: 2,
+	b: 5,
+	c: {
+		x: 7,
+		y: 4
+	}
+}
+
+const newNumbers = copy(numbers);
+
+newNumbers.a = 10;
+newNumbers.c.x = 10;
+// console.log(newNumbers);
+// console.log(numbers);
+
+
+const add = {
+	d: 17,
+	e: 20
+};
+
+const clone = Object.assign({}, add);
+
+clone.d = 20;
+// console.log(add);
+// console.log(clone);
+
+const oldArray = ['a', 'b', 'c'];
+const newArray = oldArray.slice();
+
+newArray[1] = 'asdasd';
+console.log(newArray);
+console.log(oldArray);
+
+const video = ['youtube', 'vimeo', 'rutube'],
+	blogs = ['wordpress', 'livejournal', 'blogger'],
+	internet = [...video, ...blogs, 'vk', 'facebook'];
+
+console.log(internet);
+
+function log(a, b, c) {
+	console.log(a);
+	console.log(b);
+	console.log(c);
+}
+
+const nnn = [2, 5, 7];
+
+log(...nnn);
+
+const array1 = ["a", "b"];
+const newArray1 = [...array1];
+
+const q = {
+	one: 1,
+	two: 2
+}
+
+const newObjj = {
+	...q
+};
+
+console.log(newObjj);
+
+let stri = "some";
+let striObj = new String(stri);
+
+console.log(typeof (stri));
+console.log(typeof (striObj));
+
+console.dir([1, 2, 3]);
+
+const soldier = {
+	health: 400,
+	armor: 150,
+	sayHello: function () {
+		console.log('Hello');
+	}
+};
+
+const john = Object.create(soldier);
+
+// const john = {
+// 	health: 100
+// };
+
+// john.__proto__ = soldier; //устаревший формат
+
+// Object.setPrototypeOf(john, soldier);
+
+
+
+// console.log(john.armor);
+john.sayHello();
+
+console.log(typeof (String(null)));
+
+console.log(typeof (5 + ""));
+
+
+const ns = 5;
+
+console.log("https://vk.vom/catalog/" + ns);
+
+const fontSize = 26 + 'px';
+
+console.log(typeof (Number('4')));
+console.log(typeof (+'4'));
+console.log(typeof (parseInt("15px", 10)));
+
+let answ = +prompt("Hello", "");
+
+let switcher = null;
+if (switcher) {
+	console.log("Working...");
+}
+
+switcher = 1;
+
+if (switcher) {
+	console.log("Working...");
+}
+
+console.log(typeof (Boolean('4')));
+
+console.log(typeof (!!"4444"));
